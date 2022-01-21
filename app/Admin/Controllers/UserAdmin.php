4718cdcff6078dab9dc635de593bb4688116bf42
root <lanqiukun>
2022-01-21 16:37:01 +0800

    asdf

---
+++ app/Admin/Controllers/UserAdmin.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UserAdmin extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'User';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('Id'));
+        $grid->column('openid', __('Openid'));
+        $grid->column('nickname', __('Nickname'));
+        $grid->column('avatar', __('Avatar'));
+        $grid->column('token', __('Token'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('openid', __('Openid'));
+        $show->field('nickname', __('Nickname'));
+        $show->field('avatar', __('Avatar'));
+        $show->field('token', __('Token'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->text('openid', __('Openid'));
+        $form->text('nickname', __('Nickname'));
+        $form->image('avatar', __('Avatar'));
+        $form->text('token', __('Token'));
+
+        return $form;
+    }
+}
